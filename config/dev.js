b24182d4092f52ce3c75e5ccc41adeee906a337b
max <max@gmail.com>
2022-02-21 15:22:34 +0900

    비밀정보 보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://mano:abcd1234@boilerplate.yljrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+//const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded를 분석해서 가져올 수 있게 함
+////app.use(bodyParser.urlencoded({extended: true}))
+//application/json
+//app.use(bodyParser.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://mano:abcd1234@boilerplate.yljrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+//routes
+
+app.get('/', (req, res) => res.send('Hello World~~'));
+
+app.post('/register', (req, res) => {
+    // 회원 가입 시 필요한 정보를 client에 가져오면
+    // 이것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
